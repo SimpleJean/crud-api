ad818baf4b2d33c8246017f94c2a8d326ddc3cca
Jean <zhan.yessim@gmail.com>
2022-06-16 15:59:27 +0600

    feat: added PUT and DELETE and refactor: GET by id and POST method

--- index.js
+++ index.js
@@ -17,15 +17,18 @@ const server = http.createServer((req, res) => {
   } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET') {
     try {
       const userId = req.url.split('/')[3];
-      console.log(userId);
-      const user = users.find(user => user.id === userId);
 
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify(user));
-
+      const user = users.find((user) => user.id === userId);
+      if (user) {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(user));
+      } else {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'User not found try again' }));
+      }
     } catch (err) {
-      res.writeHead(404, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ message: 'User not found' }));
+      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: 'User id is invalid' }));
     }
   } else if (req.url === '/api/users' && req.method === 'POST') {
     //work via postman POST -> Body -> raw -> json
@@ -42,24 +45,54 @@ const server = http.createServer((req, res) => {
           age,
           hobbies,
         };
-
+        console.log(body);
         users.push(user);
         writeDataFile('./data/users.json', users);
         res.writeHead(201, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify(user));
       });
-      //hardcoded users.json
-      /*const body = {
-        id: uuidv4(),
-        username: 'Vata',
-        age: '564',
-        hobbies: ['lol', 'lol', 'lol'],
-      };
-      
-      res.writeHead(201, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify(body));
+    } catch (err) {
+      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: 'does not contain required fields' }));
+    }
+    //update user data
+  } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'PUT') {
+    try {
+      const userId = req.url.split('/')[3];
+      let body = '';
+      req.on('data', (chunk) => {
+        body += chunk.toString();
 
-      writeDataFile('./data/users.json', users.concat(body)); */
+        const user = users.find((user) => user.id === userId);
+        const { username, age, hobbies } = JSON.parse(body);
+        user.username = username;
+        user.age = age;
+        user.hobbies = hobbies;
+        writeDataFile('./data/users.json', users);
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(user));
+      });
+      req.on('end', () => {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(users));
+      });
+    } catch (err) {
+      res.writeHead(500, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ error: err.message }));
+    }
+  } else if(req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'DELETE') {
+    try {
+      const userId = req.url.split('/')[3];
+      const user = users.find((user) => user.id === userId);
+      if (user) {
+        users.splice(users.indexOf(user), 1);
+        writeDataFile('./data/users.json', users);
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(user));
+      } else {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'User not found try again' }));
+      }
     } catch (err) {
       res.writeHead(500, { 'Content-Type': 'application/json' });
       res.end(JSON.stringify({ error: err.message }));
